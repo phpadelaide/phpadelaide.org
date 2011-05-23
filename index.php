8963619fcf66cbfd410811934ba319896efd187b
cphillipp <clay@delorum.com>
2011-05-23 11:29:00 -0600

    updated index

--- index.php
+++ index.php
@@ -1,18 +1,14 @@
 <?php
 // yeah... we know. This is a dummy app, cut some slack
 
-// $con = mysql_connect("localhost:/tmp/mysql/ona.sock","anya","yCq6dSl9");
-// $con = mysql_connect("127.0.0.1:3307","anya","yCq6dSl9");
-
-/*
+$con = mysql_connect("localhost:/tmp/mysql/ona.sock","anya","yCq6dSl9");
 if (!$con)
   {
+  $noDatabase = true
   die('Could not connect: ' . mysql_error());
   }
 
-mysql_select_db("janna", $con);
-$sql = "CREATE TABLE IF NOT EXISTS Message (Content text)";
-mysql_query($sql,$con);
+mysql_select_db("ona", $con);
 
 if (isset($_POST['Content'])) {
 	if ($content = $_POST['Content']) {
@@ -23,7 +19,7 @@ if (isset($_POST['Content'])) {
 		mysql_query($sql,$con);
 	}
 }
-*/
+
 ?>
 
 
@@ -39,47 +35,56 @@ if (isset($_POST['Content'])) {
 <body>
 	<div class="wrapper">
 		<div class="content">
-			<form action="index.php" method="post">
-				<div class="border">
-					<input type="text" name="Content" class="textarea" />
-				</div>
-				<input class="btn" type="image" value="" src="images/btn.png" border="0" name="btn">
-				<div class="message-box">
-					<?
-					/*					
-					$count  = 0;
-					$cssClass  = array("yellow", "orange");
-					$result = mysql_query("SELECT * FROM Message");
-					$valAr = array();
-					
-					// Pull values out of db and put in array
-					while($row = mysql_fetch_array($result))
-					{
-						array_push( $valAr, $row['Content'] );			 		    
-					}
-					
-					// Reverse arry and print values as html
-					$valAr = array_reverse( $valAr );
-					$len = count($valAr);
-					for ($i=0; $i < $len; $i++) { 
-						echo  "<div class='message " . $cssClass[$count] . "'>";
-						echo  "	<span class='top'></span>";
-						echo  $valAr[$i];
-						echo  "	<span class='bottom'></span>";
-						echo  "</div>";
-						
-						$count++;
-						if( $count == ( count($cssClass) ) ){
-							$count = 0;
-						}
-					}
-					
-					
-					mysql_close($con);
-					*/
-					?>
-				</div>
-			</form>
+		  <? if ($noDatabase): ?>
+		    <div class="moon"></div>
+		    <div class="box">
+		      <p>We were unable to locate your database. You can create one in your <a href="#link">admin panel</a>, then simply set the following global vars to your database’s credentials. You do so via the <a href="#link">Global Vars</a> tab in the admin panel:</p>
+		      <p class="indent">db_name = &lsaquo;your-db-name&rsaquo;</p>
+		      <p class="indent">db_pass = &lsaquo;your-db-password&rsaquo;</p>
+		      <p class="indent">db_host = &lsaquo;your-db-host&rsaquo;</p>
+		      <p>No need to move in haste, the universe is patient.</p>
+		    </div>
+		  <? else: ?>
+		    <form action="index.php" method="post">
+  				<div class="border">
+  					<input type="text" name="Content" class="textarea" />
+  				</div>
+  				<input class="btn" type="image" value="" src="images/btn.png" border="0" name="btn">
+  				<div class="message-box">
+  					<?					
+  					$count  = 0;
+  					$cssClass  = array("yellow", "orange");
+  					$result = mysql_query("SELECT * FROM Message");
+  					$valAr = array();
+
+  					// Pull values out of db and put in array
+  					while($row = mysql_fetch_array($result))
+  					{
+  						array_push( $valAr, $row['Content'] );			 		    
+  					}
+
+  					// Reverse arry and print values as html
+  					$valAr = array_reverse( $valAr );
+  					$len = count($valAr);
+  					for ($i=0; $i < $len; $i++) { 
+  						echo  "<div class='message " . $cssClass[$count] . "'>";
+  						echo  "	<span class='top'></span>";
+  						echo  $valAr[$i];
+  						echo  "	<span class='bottom'></span>";
+  						echo  "</div>";
+
+  						$count++;
+  						if( $count == ( count($cssClass) ) ){
+  							$count = 0;
+  						}
+  					}
+
+
+  					mysql_close($con);
+  					?>
+  				</div>
+  			</form>
+		  <? endif ?>
 		</div>
 	</div>
 </body>
