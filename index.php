d07bf4af8d25265c3465a3af11d3480a23fdb1f2
cphillipp <clay@delorum.com>
2011-05-23 11:49:30 -0600

    changed false

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // yeah... we know. This is a dummy app, cut some slack
-$noDatabase = true;
+$noDatabase = false;
 if (isset($_SERVER['db_host']) && isset($_SERVER['db_user']) && isset($_SERVER['db_pass'])) {
   $con = mysql_connect($_SERVER['db_host'],$_SERVER['db_user'],$_SERVER['db_pass']);
   if (!$con)
