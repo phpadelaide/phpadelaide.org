bbb6534bbe29f80edc58e3d2ab8a50568dad1913
Pascal Opitz <pascalopitz@pascalopitz-VirtualBox.(none)>
2012-04-04 14:47:20 +0930

    changed number of tweets displayed

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $app->get('/', function() use($app) { 
     
-    $searchUrl = 'http://search.twitter.com/search.json?q=%23phpadelaide&rpp=10&include_entities=true&result_type=mixed';
+    $searchUrl = 'http://search.twitter.com/search.json?q=%23phpadelaide&rpp=100&include_entities=true&result_type=mixed';
     $twitterResponse = json_decode(file_get_contents($searchUrl), true);
     
     return $app['twig']->render('index.html.twig', array(
