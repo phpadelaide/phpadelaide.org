f2d0a6a03d75df2fada4197167d79814e0156505
Pascal Opitz <pascalopitz@pascalopitz-VirtualBox.(none)>
2012-04-05 08:27:16 +0930

    rejuggled the app layout

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,13 +5,13 @@ if(!isset($_SERVER['APP_MYSQL_DB']))          $_SERVER['APP_MYSQL_DB'] = 'phpade
 if(!isset($_SERVER['APP_MYSQL_PASSWORD']))    $_SERVER['APP_MYSQL_PASSWORD'] = '';
 if(!isset($_SERVER['APP_MYSQL_USER']))        $_SERVER['APP_MYSQL_USER'] = 'root';
 
-require_once __DIR__.'/silex.phar'; 
+require_once __DIR__.'/../vendor/silex/silex.phar'; 
 
 $app = new Silex\Application(); 
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path'       => __DIR__.'/views',
-    'twig.class_path' => __DIR__.'/vendor/twig/lib',
+    'twig.path'       => __DIR__.'/../views',
+    'twig.class_path' => __DIR__.'/../vendor/twig/lib',
 ));
 
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
@@ -22,6 +22,8 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
         'user'      => $_SERVER['APP_MYSQL_USER'],
         'password'  => $_SERVER['APP_MYSQL_PASSWORD'],
     ),
-    'db.dbal.class_path'    => __DIR__.'/vendor/doctrine-dbal/lib',
-    'db.common.class_path'  => __DIR__.'/vendor/doctrine-common/lib',
+    'db.dbal.class_path'    => __DIR__.'/../vendor/doctrine-dbal/lib',
+    'db.common.class_path'  => __DIR__.'/../vendor/doctrine-common/lib',
 ));
+
+return $app;
